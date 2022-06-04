ec8d82a86b224cbf04e0ecc9e277ebcd70123bfd
NicoAmont <nicolas.amezquita.m@gmail.com>
2022-06-04 13:23:21 -0500

    :dizzy_face: fixing production bugs

--- src/controllers/MutantCotroller.js
+++ src/controllers/MutantCotroller.js
@@ -11,26 +11,26 @@ const isMutant = async function(req, res) {
     var dna = data.dna;
     let dna_db = [];
 
-    await isMutantDna(dna).then(async function(){
-        dna_db = await Human.find({dna:dna.join('')});
-        if(dna_db.length == 0) {
-            var humanDna = { dna: dna.join('') , isMutant: verification }
-            var reg = await Human.create(humanDna);
-            
-            if(!verification){
-                res.status(403).send({status: 403, statusText: 'Forbidden'})
-            } else{      
-                res.status(200).send({status: 200, statusText: 'OK'})
-            }
-            
-        }else{
-            
-            res.status(200).send({message:'DNA is already in the DB',data:undefined});
+    let verification = await isMutantDna(dna);
+    dna_db = await Human.find({dna:dna.join('')});
+    
+    
+    if(dna_db.length == 0) {
+        var humanDna = { dna: dna.join('') , isMutant: verification }
+        var reg = await Human.create(humanDna);
+        
+        if(!verification){
+            res.status(403).send({status: 403, statusText: 'Forbidden'})
+        } else{      
+            res.status(200).send({status: 200, statusText: 'OK'})
         }
+        
+    }else{
+        
+        res.status(200).send({message:'DNA is already in the DB',data:undefined});
+    }
+   
 
-    }).catch(function(err){
-        console.log(err)
-    })
    
 
     
